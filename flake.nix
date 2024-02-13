e2f2a73fa45f77a338c147dd07e8e68dc0cc0cd3
pancaek <braxtonrboer@gmail.com>
2024-02-12 21:32:06 -0800

    home manager?

--- flake.nix
+++ flake.nix
@@ -1,13 +1,30 @@
 {
   description = "My flakes configuration";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; };
+  inputs = {
 
-  outputs = { self, nixpkgs }@inputs: {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }@inputs: {
     nixosConfigurations = {
       pancake-nix = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [ ./configuration.nix ];
+      };
+
+      homeConfigurations = {
+        pancaek = home-manager.lib.homeManagerConfiguration {
+          # Note: I am sure this could be done better with flake-utils or something
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+          modules = [ ./home.nix ]; # Defined later
+        };
+
       };
     };
   };
