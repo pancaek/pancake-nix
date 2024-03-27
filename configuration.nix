8ea5a140909574803b366424bbca03dc6075d81a
pancaek <devynrboer@gmail.com>
2024-03-27 16:47:48 -0700

    move gnome stuff to its own file

--- configuration.nix
+++ configuration.nix
@@ -80,12 +80,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver = {
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-  };
-
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
