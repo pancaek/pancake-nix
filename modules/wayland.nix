2d44a1ff3e45aabbc11ef42ee229fee38b12a57e
pancaek <20342389+pancaek@users.noreply.github.com>
2024-06-18 15:02:54 -0700

    adjust configuration.nix and modules

---
+++ modules/wayland.nix
@@ -0,0 +1,21 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  # Shorter name to access final settings a 
+  # user of hello.nix module HAS ACTUALLY SET.
+  # cfg is a typical convention.
+  cfg = config.modules.wayland;
+in
+{
+  # Declare what settings a user of this module can set.
+  options.modules.wayland = {
+    enable = mkEnableOption "Enable options to make Wayland play nice";
+  };
+
+  # Define what other settings, services and resources should be active IF
+  # a user of this "hello.nix" module ENABLED this module 
+  # by setting "services.hello.enable = true;".
+  config = mkIf cfg.enable {
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  };
+}
