78a3adebddbff4a47c073e499bb996ff81696e15
pancaek <20342389+pancaek@users.noreply.github.com>
2024-06-26 12:02:15 -0700

    make modules

---
+++ modules/audio.nix
@@ -0,0 +1,41 @@
+
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  # Shorter name to access final settings a
+  # user of hello.nix module HAS ACTUALLY SET.
+  # cfg is a typical convention.
+  cfg = config.modules.audio;
+in
+{
+  # Declare what settings a user of this module can set.
+  options.modules.audio = {
+    enable = mkEnableOption "Enable options to for audio";
+  };
+
+  # Define what other settings, services and resources should be active IF
+  # a user of this "hello.nix" module ENABLED this module
+  # by setting "services.hello.enable = true;".
+  config = mkIf cfg.enable {
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  };
+}
