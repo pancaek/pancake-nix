5bfd9eb3c2cbc939c878acca73e7230a20e4051f
pancaek <20342389+pancaek@users.noreply.github.com>
2024-07-08 11:38:23 -0700

    remove a bunch of with lib;

--- modules/kvantum.nix
+++ modules/kvantum.nix
@@ -4,7 +4,6 @@
   config,
   ...
 }:
-with lib;
 let
   # Shorter name to access final settings a
   # user of hello.nix module HAS ACTUALLY SET.
@@ -14,13 +13,13 @@ in
 {
   # Declare what settings a user of this module can set.
   options.programs.kvantum = {
-    enable = mkEnableOption "kvantum";
+    enable = lib.mkEnableOption "kvantum";
   };
 
   # Define what other settings, services and resources should be active IF
   # a user of this "hello.nix" module ENABLED this module
   # by setting "services.hello.enable = true;".
-  config = mkIf cfg.enable {
+  config = lib.mkIf cfg.enable {
     qt.enable = true;
     qt.platformTheme = "qt5ct";
     qt.style = "kvantum";
