a09ea78b31a7bea6062a042a48b68c86cdcc7559
pancaek <20342389+pancaek@users.noreply.github.com>
2024-07-12 04:30:20 -0700

    modularize and and unstable overlay

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
 
     nixpkgs.url = "nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -15,24 +16,72 @@
     {
       self,
       nixpkgs,
+      nixpkgs-unstable,
       home-manager,
       nur,
     }:
     let
       system = "x86_64-linux";
+      overlay-unstable = final: prev: {
+        # use this variant if unfree packages are needed:
+        unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
     in
     {
       nixosConfigurations = {
         pancake-laptop = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [
+            (
+              { config, pkgs, ... }:
+              {
+                nixpkgs.overlays = [ overlay-unstable ];
+              }
+            )
             nur.nixosModules.nur
-            ./configuration.nix
+            ./hosts/laptop/configuration.nix
             ./modules/quiet-boot.nix
             ./modules/audio.nix
             ./modules/printing.nix
             ./modules/wayland.nix
             ./modules/hardware/laptop.nix
+            # ./modules/xdg-compliance.nix # TODO another day
+            ./modules/desktops/pancake-gnome.nix
+
+            # make home-manager as a module of nixos
+            # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.pancaek = import ./home/home.nix;
+              };
+
+              # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+            }
+          ];
+        };
+
+        pancake-pc = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            (
+              { config, pkgs, ... }:
+              {
+                nixpkgs.overlays = [ overlay-unstable ];
+              }
+            )
+            nur.nixosModules.nur
+            ./hosts/desktop/configuration.nix
+            ./modules/quiet-boot.nix
+            ./modules/audio.nix
+            ./modules/printing.nix
+            ./modules/wayland.nix
+            ./modules/hardware/desktop.nix
             # ./modules/xdg-compliance.nix # TODO another day
             ./modules/desktops/pancake-gnome.nix
 
