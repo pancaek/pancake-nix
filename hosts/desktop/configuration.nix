34f906c2d80558d730c18fcaf6a3341297cfa1b9
pancaek <20342389+pancaek@users.noreply.github.com>
2024-07-12 04:30:20 -0700

    modularize and and unstable overlay

---
+++ hosts/desktop/configuration.nix
@@ -0,0 +1,199 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  nix = {
+    # package = pkgs.nixUnstable;
+    optimise.automatic = true;
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+    };
+    grub = {
+      efiSupport = true;
+      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+      device = "nodev";
+      # useOSProber = true;
+    };
+  };
+  networking.hostName = "pancake-pc"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Make wayland play nice maybe hopefully
+  modules.wayland.enable = false;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Bluetooth
+  hardware.bluetooth = {
+    enable = true; # enables support for Bluetooth
+    powerOnBoot = true; # powers up the default Bluetooth controller on boot
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+  # Wonky in Wayland
+  i18n.inputMethod = {
+    enabled = "ibus";
+    ibus = {
+      engines = with pkgs.ibus-engines; [ mozc ];
+    };
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+    options = "compose:menu";
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.pancaek = {
+    isNormalUser = true;
+    description = "Pancake";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+
+  users.defaultUserShell = pkgs.zsh;
+  programs.zsh.enable = true;
+
+  modules.quiet-boot.enable = true;
+  modules.audio.enable = true;
+  modules.printing.enable = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = (
+    with pkgs;
+    [
+      git
+      adw-gtk3
+      (papirus-icon-theme.override { color = "red"; })
+      volantes-cursors
+      neofetch
+      qmk
+      gh
+      vesktop
+      (mpv.override { scripts = [ mpvScripts.uosc ]; })
+    ]
+    ++ [
+      zsh-completions
+      zsh-autosuggestions
+      zsh-history-substring-search
+      zsh-syntax-highlighting
+      bat
+      eza
+      nixd
+      nixfmt-rfc-style
+      nixpkgs-fmt
+    ]
+    ++ [ config.nur.repos.nltch.spotify-adblock ]
+  );
+
+  fonts.packages = (
+    with pkgs;
+    [
+      meslo-lgs-nf
+      noto-fonts
+      noto-fonts-cjk
+    ]
+  );
+
+  modules.pancake-gnome.enable = true;
+
+  # Enableable programs
+  # TODO:  Fix hardware acceleration
+  # XXX: Home manager extensions / ui tweaks (tabmanager)
+  programs.firefox.enable = true;
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+  programs.piper.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+
+  # Only needed for the VM
+  # services.spice-vdagentd.enable = true;
+}
