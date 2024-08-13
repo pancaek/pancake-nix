f1597910f8aed2892594aa351aa43445c3b4b3fe
pancaek <20342389+pancaek@users.noreply.github.com>
2024-08-13 14:30:16 -0700

    better quiet boot

--- modules/quiet-boot.nix
+++ modules/quiet-boot.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, config, ...}:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 # with lib;
 let
   # Shorter name to access final settings a
@@ -17,10 +22,14 @@ in
   # by setting "services.hello.enable = true;".
   config = lib.mkIf cfg.enable {
     boot.plymouth.enable = true;
+    boot.initrd.verbose = false;
     boot.kernelParams = [
       "quiet"
       "splash"
-      "vga=current"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
       "udev.log_priority=3"
     ];
   };
