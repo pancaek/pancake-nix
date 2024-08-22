b4ee3d1482b9d42409339e6fefd18be1209ffe9e
pancaek <20342389+pancaek@users.noreply.github.com>
2024-08-22 15:15:55 -0700

    common nix for desktop

--- hosts/common.nix
+++ hosts/common.nix
@@ -154,9 +154,6 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
