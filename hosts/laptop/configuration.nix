1dbda0aff264921c0a6d0f36ae789021adc41abb
pancaek <20342389+pancaek@users.noreply.github.com>
2024-08-22 18:14:04 -0700

    common nix for laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -14,16 +14,8 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../common.nix
   ];
-
-  nix = {
-    # package = pkgs.nixUnstable;
-    optimise.automatic = true;
-    settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-  };
 
   boot.loader = {
     efi = {
@@ -43,117 +35,7 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Bluetooth
-  hardware.bluetooth = {
-    enable = true; # enables support for Bluetooth
-    powerOnBoot = true; # powers up the default Bluetooth controller on boot
-  };
-
-  # Set your time zone.
-  time.timeZone = "America/Los_Angeles";
-
-  # Select internationalisation properties.
-  i18n = {
-    defaultLocale = "en_US.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "en_US.UTF-8";
-      LC_IDENTIFICATION = "en_US.UTF-8";
-      LC_MEASUREMENT = "en_US.UTF-8";
-      LC_MONETARY = "en_US.UTF-8";
-      LC_NAME = "en_US.UTF-8";
-      LC_NUMERIC = "en_US.UTF-8";
-      LC_PAPER = "en_US.UTF-8";
-      LC_TELEPHONE = "en_US.UTF-8";
-      LC_TIME = "en_US.UTF-8";
-    };
-  };
-
-  # Fix localectl
-  services.xserver.exportConfiguration = true;
-
-  modules.ibus = {
-    enable = true;
-    engines = with pkgs.ibus-engines; [ mozc ];
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Configure keymap in X11
-  console.useXkbConfig = true;
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-    options = "compose:menu";
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.pancaek = {
-    isNormalUser = true;
-    description = "Pancake";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-  };
-
-  # ZSH
-  users.defaultUserShell = pkgs.zsh;
-  programs.zsh.enable = true;
-  # NOTE: Enabled by default in programs.zsh.enable (needed for completions from home-manager)
-  # environment.pathsToLink = [ "/share/zsh" ];
-
-  modules.quiet-boot.enable = true;
-  modules.audio.enable = true;
-  modules.printing.enable = true;
-
-  modules.pancake-gnome.enable = true;
-
-  # XXX: Home manager extensions / ui tweaks (tabmanager)
-  modules.firefox.enable = true;
-
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-
-  # NOTE: QMK udev rules
-  hardware.keyboard.qmk.enable = true;
-
-  environment.systemPackages = (
-    with pkgs;
-    [
-      git
-      adw-gtk3
-      (papirus-icon-theme.override { color = "red"; })
-      volantes-cursors
-      neofetch
-      qmk
-      gh
-      (mpv.override { scripts = [ mpvScripts.uosc ]; })
-    ]
-    ++ [
-      nixd
-      nixfmt-rfc-style
-    ]
-    ++ [ config.nur.repos.nltch.spotify-adblock ]
-  );
-
-  fonts.packages = (
-    with pkgs;
-    [
-      meslo-lgs-nf
-      noto-fonts
-      noto-fonts-cjk
-    ]
-  );
+  environment.systemPackages = (with pkgs; [ fastfetch ]);
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
