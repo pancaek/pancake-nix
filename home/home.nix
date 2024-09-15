7a9534954872b9a1c77438b247ef98a329548c2f
pancaek <20342389+pancaek@users.noreply.github.com>
2024-09-14 22:46:07 -0700

    patch reaper itself

--- home/home.nix
+++ home/home.nix
@@ -19,53 +19,12 @@
     obs-studio
     vscode-fhs
     (reaper.overrideAttrs (prev: {
-      installPhase =
-        if stdenv.isDarwin then
-          ''
-            runHook preInstall
-            mkdir -p "$out/Applications/Reaper.app"
-            cp -r * "$out/Applications/Reaper.app/"
-            makeWrapper "$out/Applications/Reaper.app/Contents/MacOS/REAPER" "$out/bin/reaper"
-            runHook postInstall
-          ''
-        else
-          ''
-            runHook preInstall
-
-            HOME="$out/share" XDG_DATA_HOME="$out/share" ./install-reaper.sh \
-              --install $out/opt \
-              --integrate-user-desktop
-            rm $out/opt/REAPER/uninstall-reaper.sh
-
-            rm $out/opt/REAPER/libSwell.so
-            ln -s ${libswell}/lib/libSwell.so $out/opt/REAPER/libSwell.so
-
-            # Dynamic loading of plugin dependencies does not adhere to rpath of
-            # reaper executable that gets modified with runtimeDependencies.
-            # Patching each plugin with DT_NEEDED is cumbersome and requires
-            # hardcoding of API versions of each dependency.
-            # Setting the rpath of the plugin shared object files does not
-            # seem to have an effect for some plugins.
-            # We opt for wrapping the executable with LD_LIBRARY_PATH prefix.
-            # Note that libcurl and libxml2 are needed for ReaPack to run.
-            wrapProgram $out/opt/REAPER/reaper \
-              --prefix LD_LIBRARY_PATH : "${
-                lib.makeLibraryPath [
-                  curl
-                  lame
-                  libxml2
-                  ffmpeg
-                  vlc
-                  xdotool
-                  stdenv.cc.cc.lib
-                ]
-              }"
-
-            mkdir $out/bin
-            ln -s $out/opt/REAPER/reaper $out/bin/
-
-            runHook postInstall
-          '';
+      postInstall =
+        (prev.postInstall or "")
+        + ''
+          rm $out/opt/REAPER/libSwell.so
+          ln -s ${libswell}/lib/libSwell.so $out/opt/REAPER/libSwell.so
+        '';
     }))
     vesktop
   ];
