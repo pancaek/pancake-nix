cf145125da7904d979415e8aa160e9bed047da38
pancaek <20342389+pancaek@users.noreply.github.com>
2024-09-14 20:37:14 -0700

    patch reaper libSwell.so to use non-hardcoded xdg-open

---
+++ pkgs/by-name/libswell/package.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  makeWrapper,
+  pkg-config,
+  gtk3,
+}:
+
+stdenv.mkDerivation {
+  pname = "libswell";
+  version = "unstable-2024-09-13";
+
+  src = fetchFromGitHub {
+    owner = "justinfrankel";
+    repo = "WDL";
+    rev = "12c86f9061c0c517bb768c2593eb36670f34eb4d";
+    hash = "sha256-om2bwIgFgo3F8QCeSL2W5ykThA9AAYq/3/kZPRII45Q=";
+  };
+  buildInputs = [
+    makeWrapper
+    pkg-config
+    gtk3
+  ];
+
+  buildPhase = ''
+    cd WDL/swell
+    make
+  '';
+
+  installPhase = ''
+    mkdir -p $out/lib
+    cp libSwell.so $out/lib/libSwell.so
+
+  '';
+
+  patches = [ ./libswell-patch.diff ];
+
+  meta = with lib; {
+    description = "WDL (by Cockos) mirror";
+    homepage = "https://github.com/justinfrankel/WDL";
+    license = licenses.unfree; # FIXME: nix-init did not found a license
+    maintainers = with maintainers; [ ];
+    platforms = platforms.all;
+  };
+}
