e701d9eb7adc1aeb43d4db3957726b249ae95f95
pancaek <20342389+pancaek@users.noreply.github.com>
2024-10-11 17:32:13 -0700

    piper 0.8

--- modules/piper.nix
+++ modules/piper.nix
@@ -8,48 +8,41 @@ let
   # Shorter name to access final settings a
   # user of hello.nix module HAS ACTUALLY SET.
   # cfg is a typical convention.
+
   cfg = config.programs.piper;
 
-  piperPackage =
-    if !cfg.experimental then
-      pkgs.piper
-    else
-      pkgs.piper.overrideAttrs (
-        prev:
-        let
-          version = "0.8";
-        in
-        {
-          inherit version;
-          src = pkgs.fetchFromGitHub {
-            inherit (prev.src) owner repo;
-            rev = version;
-            hash = "sha256-j58fL6jJAzeagy5/1FmygUhdBm+PAlIkw22Rl/fLff4=";
-          };
-          # Remove "-Dtests=false" flag that doesn't exist in the new version
-          # https://github.com/libratbag/piper/commit/a8ba2124318cc12477ffee932c7ae9c3614926c2
-          mesonFlags = lib.remove "-Dtests=false" prev.mesonFlags;
-        }
-      );
+  piperPackage = lib.overrideAttrsIf (cfg.experimental) (
+    prev:
+    let
+      version = "0.8";
+    in
+    {
+      inherit version;
+      src = pkgs.fetchFromGitHub {
+        inherit (prev.src) owner repo;
+        rev = version;
+        hash = "sha256-j58fL6jJAzeagy5/1FmygUhdBm+PAlIkw22Rl/fLff4=";
+      };
+      # Remove "-Dtests=false" flag that doesn't exist in the new version
+      # https://github.com/libratbag/piper/commit/a8ba2124318cc12477ffee932c7ae9c3614926c2
+      mesonFlags = lib.remove "-Dtests=false" prev.mesonFlags;
+    }
+  ) pkgs.piper;
 
-  ratbagPackage =
-    if !cfg.experimental then
-      pkgs.libratbag
-    else
-      pkgs.libratbag.overrideAttrs (
-        prev:
-        let
-          version = "0.18";
-        in
-        {
-          inherit version;
-          src = pkgs.fetchFromGitHub {
-            inherit (prev.src) owner repo;
-            rev = "v${version}";
-            hash = "sha256-dAWKDF5hegvKhUZ4JW2J/P9uSs4xNrZLNinhAff6NSc=";
-          };
-        }
-      );
+  ratbagPackage = lib.overrideAttrsIf (cfg.experimental) (
+    prev:
+    let
+      version = "0.18";
+    in
+    {
+      inherit version;
+      src = pkgs.fetchFromGitHub {
+        inherit (prev.src) owner repo;
+        rev = "v${version}";
+        hash = "sha256-dAWKDF5hegvKhUZ4JW2J/P9uSs4xNrZLNinhAff6NSc=";
+      };
+    }
+  ) pkgs.libratbag;
 in
 {
   # Declare what settings a user of this module can set.
