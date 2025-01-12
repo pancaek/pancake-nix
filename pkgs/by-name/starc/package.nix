393ce0cfeb8d8ba8c2f0bd334204f2b785517bd8
pancaek <20342389+pancaek@users.noreply.github.com>
2025-01-11 22:14:25 -0800

    refactor starc now that it works

--- pkgs/by-name/starc/package.nix
+++ pkgs/by-name/starc/package.nix
@@ -4,34 +4,32 @@
   fetchurl,
   makeWrapper,
 }:
+let
 
-appimageTools.wrapType2 rec {
   pname = "starc";
   version = "0.7.5";
-
   src = fetchurl {
     url = "https://github.com/story-apps/starc/releases/download/v${version}/starc-setup.AppImage";
     hash = "sha256-KAY04nXVyXnjKJxzh3Pvi50Vs0EPbLk0VgfZuz7MQR0=";
   };
 
+  appimageContents = appimageTools.extract { inherit pname version src; };
+in
+appimageTools.wrapType2 {
+  inherit pname version src;
   nativeBuildInputs = [ makeWrapper ];
-  extraInstallCommands =
-    let
-      appimageContents = appimageTools.extract { inherit pname version src; };
-    in
-    ''
-      # Fixup desktop item icons
-      install -D ${appimageContents}/starc.desktop -t $out/share/applications/
-      substituteInPlace $out/share/applications/starc.desktop \
-      --replace-fail "Icon=starc" "${''
-        Icon=dev.storyapps.starc
-        StartupWMClass=Story Architect''}"
-      cp -r ${appimageContents}/share/* $out/share/
-
-      wrapProgram $out/bin/starc \
-        --unset QT_PLUGIN_PATH
-
-    '';
+  extraInstallCommands = ''
+    wrapProgram $out/bin/starc \
+      --unset QT_PLUGIN_PATH
+      
+    # Fixup desktop item icons
+    install -D ${appimageContents}/starc.desktop -t $out/share/applications/
+    substituteInPlace $out/share/applications/starc.desktop \
+    --replace-fail "Icon=starc" "${''
+      Icon=dev.storyapps.starc
+      StartupWMClass=Story Architect''}"
+    cp -r ${appimageContents}/share/* $out/share/
+  '';
 
   meta = {
     description = "Intuitive screenwriting app that streamlines the writing process";
