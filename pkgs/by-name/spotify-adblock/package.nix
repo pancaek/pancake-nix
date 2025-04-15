5889c414adad329f1f449cc17fbe9f8a9735da82
pancaek <20342389+pancaek@users.noreply.github.com>
2025-04-15 13:30:40 -0700

    revert spotify-adblock refactor, i broke it

--- pkgs/by-name/spotify-adblock/package.nix
+++ pkgs/by-name/spotify-adblock/package.nix
@@ -6,50 +6,36 @@
   xorg,
   zip,
   unzip,
-  lib,
 }:
 let
-  spotify-adblock =
-    let
-      version = "1.0.3";
-    in
-    rustPlatform.buildRustPackage {
-      pname = "spotify-adblock";
-      inherit version;
-      src = fetchFromGitHub {
-        owner = "abba23";
-        repo = "spotify-adblock";
-        tag = "v${version}";
-        hash = "sha256-UzpHAHpQx2MlmBNKm2turjeVmgp5zXKWm3nZbEo0mYE=";
-      };
-      cargoHash = "sha256-wPV+ZY34OMbBrjmhvwjljbwmcUiPdWNHFU3ac7aVbIQ=";
-
-      patchPhase = ''
-        substituteInPlace src/lib.rs \
-          --replace 'config.toml' $out/etc/spotify-adblock/config.toml
-      '';
-
-      buildPhase = ''
-        make
-      '';
-
-      installPhase = ''
-        install -Dm644 config.toml $out/etc/spotify-adblock
-        install -Dsm644 target/release/libspotifyadblock.so -t $out/lib
-      '';
-
-    };
-
-  spotifywm = stdenv.mkDerivation {
-    name = "spotifywm";
+  spotify-adblock = rustPlatform.buildRustPackage {
+    pname = "spotify-adblock";
+    version = "1.0.3";
     src = fetchFromGitHub {
-      owner = "dasj";
-      repo = "spotifywm";
-      rev = "8624f539549973c124ed18753881045968881745";
-      hash = "sha256-AsXqcoqUXUFxTG+G+31lm45gjP6qGohEnUSUtKypew0=";
+      owner = "abba23";
+      repo = "spotify-adblock";
+      rev = "5a3281dee9f889afdeea7263558e7a715dcf5aab";
+      hash = "sha256-UzpHAHpQx2MlmBNKm2turjeVmgp5zXKWm3nZbEo0mYE=";
     };
-    buildInputs = [ xorg.libX11 ];
-    installPhase = "install -Dm644 spotifywm.so -t $out/lib";
+    cargoHash = "sha256-wPV+ZY34OMbBrjmhvwjljbwmcUiPdWNHFU3ac7aVbIQ=";
+
+    patchPhase = ''
+      substituteInPlace src/lib.rs \
+        --replace 'config.toml' $out/etc/spotify-adblock/config.toml
+    '';
+
+    buildPhase = ''
+      make
+    '';
+
+    installPhase = ''
+      mkdir -p $out/etc/spotify-adblock
+      install -D --mode=644 config.toml $out/etc/spotify-adblock
+      mkdir -p $out/lib
+      install -D --mode=644 --strip target/release/libspotifyadblock.so $out/lib
+
+    '';
+
   };
 in
 spotify.overrideAttrs (old: {
@@ -58,21 +44,16 @@ spotify.overrideAttrs (old: {
     unzip
   ];
   postInstall =
-    let
-      ld = lib.concatStringsSep " " [
-        "${spotify-adblock}/lib/libspotifyadblock.so"
-        "${spotifywm}/lib/spotifywm.so"
-      ];
-    in
     (old.postInstall or "")
     + ''
       ln -s ${spotify-adblock}/lib/libspotifyadblock.so $libdir
       sed -i "s:^Name=Spotify.*:Name=Spotify-adblock:" "$out/share/spotify/spotify.desktop"
       wrapProgram $out/bin/spotify \
-        --set LD_PRELOAD "${ld}"
+        --set LD_PRELOAD "${spotify-adblock}/lib/libspotifyadblock.so"
+
       # Hide placeholder for advert banner
-      ${lib.getExe unzip} -p $out/share/spotify/Apps/xpui.spa xpui.js | sed 's/adsEnabled:\!0/adsEnabled:false/' > $out/share/spotify/Apps/xpui.js
-      ${lib.getExe zip} --junk-paths --update $out/share/spotify/Apps/xpui.spa $out/share/spotify/Apps/xpui.js
+      ${unzip}/bin/unzip -p $out/share/spotify/Apps/xpui.spa xpui.js | sed 's/adsEnabled:\!0/adsEnabled:false/' > $out/share/spotify/Apps/xpui.js
+      ${zip}/bin/zip --junk-paths --update $out/share/spotify/Apps/xpui.spa $out/share/spotify/Apps/xpui.js
       rm $out/share/spotify/Apps/xpui.js
     '';
 })
