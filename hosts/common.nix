2fc78b9c10df3b474b3d910af63bc32d43d563da
pancaek <20342389+pancaek@users.noreply.github.com>
2025-08-13 16:54:21 -0700

    fix steam cursor

--- hosts/common.nix
+++ hosts/common.nix
@@ -111,6 +111,9 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    extraPackages = with pkgs; [
+      volantes-cursors
+    ];
   };
 
   environment.systemPackages = (
